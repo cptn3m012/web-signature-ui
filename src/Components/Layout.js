df80b9866a130528eea331e985f8531629953c4c
trimplexx <lkrawczyk2001@gmail.com>
2023-11-12 22:30:16 +0100

    Footer repair

--- src/Components/Layout.js
+++ src/Components/Layout.js
@@ -1,17 +1,16 @@
 import React, { Component } from 'react';
 import { Container } from 'reactstrap';
-import  NavMenu  from './NavMenu';
+import NavMenu from './NavMenu';
 import Footer from "./Footer";
-
 
 export class Layout extends Component {
     static displayName = Layout.name;
 
     render() {
         return (
-            <div>
+            <div className="d-flex flex-column min-vh-100">
                 <NavMenu />
-                <Container tag="main">
+                <Container tag="main" className="flex-grow-1">
                     {this.props.children}
                 </Container>
                 <Footer/>
